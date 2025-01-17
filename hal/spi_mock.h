d15e045131d86d3c458b04bf24434c7da23f7012
Elan456 <telan4892@gmail.com>
2025-01-17 17:40:00 -0500

    Add a bunch more transfer signatures

--- hal/spi_mock.h
+++ hal/spi_mock.h
@@ -12,17 +12,32 @@ class SPISettings{
         SPISettings(int a, int b, int c){}
 };
 
-// Define all the SPI function to do nothing
-class MockSPI{
+// TODO: Add vectors to keep track of what data is being sent for better testing
+class MockSPIClass{
     public:
+        MockSPIClass(uint32_t& mosi, uint32_t& miso, uint32_t& sclk, uint32_t& ssel){} // Do nothing constructor
+        MockSPIClass(){} // Do nothing constructor
         static void begin(){}
         static void end(){}
         static void beginTransaction(SPISettings s){}
-        static void transfer(int a){}
+        static bool transfer(uint8_t& data, int length) { return true; } // Do nothing transfer
+        static bool transfer(uint8_t data) { return true; } // Do nothing transfer
+        static bool transfer(uint8_t* data, std::nullptr_t np, int length) { return true; } // Do nothing transfer
+        static bool transfer(uint8_t* a, uint8_t*& b, size_t& c) { return true; } // Do nothing transfer
+        static bool transfer(const uint8_t* data, std::nullptr_t np, int length) { return true; } // Do nothing transfer
+        static bool transfer(uint8_t* a, uint8_t b, int c) { return true; } // New overload for int
+        static bool transfer(uint8_t* a, uint8_t* b, int c) { return true; } // New overload for int
+        static bool transfer(uint8_t* a, uint8_t* b, int* c) { return true; } // New overload for int
 };
 
-MockSPI SPI; // This is the mock SPI object
+// To allow for the use of the SPI class with a line like `SPIClass flashSpi;`
+class SPIClass : public MockSPIClass {
+public:
+    SPIClass(uint32_t& mosi, uint32_t& miso, uint32_t& sclk, uint32_t& ssel)
+        : MockSPIClass(mosi, miso, sclk, ssel) {} // Forwarding constructor
+    SPIClass() : MockSPIClass() {} // Forwarding constructor
+};
 
-
+SPIClass SPI; // This is the mock SPI object
 
 #endif
