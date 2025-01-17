296ad2e5bc4e93f1573343f753733b8cbc589bcd
Elan456 <telan4892@gmail.com>
2025-01-17 17:28:55 -0500

    Add delay to ArduinoHAL

--- hal/ArduinoHAL.h
+++ hal/ArduinoHAL.h
@@ -13,6 +13,8 @@
 // Within here we must define the mock functions for the Arduino functions
 #include "serial_mock.h"
 
+#include <thread>
+
 #define OUTPUT 1
 #define INPUT 0
 #define HIGH 1
@@ -31,6 +33,11 @@ void digitalWrite(int pin, int value) {
 static auto program_start = std::chrono::high_resolution_clock::now();
 unsigned long millis() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - program_start).count();
+}
+
+void delay(unsigned long ms) {
+    // Wait using the real time clock
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
 
