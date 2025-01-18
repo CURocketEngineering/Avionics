c43e8940e32315cfab18f1669122f804d55b5eae
Elan456 <telan4892@gmail.com>
2025-01-18 17:19:09 -0500

    Make function declarations in ArduinoHAL inline

--- hal/ArduinoHAL.h
+++ hal/ArduinoHAL.h
@@ -4,6 +4,7 @@
 #ifdef ARDUINO
 #include "Arduino.h"
 #include <SPI.h>
+#include <Adafruit_SPIFlash.h>
 #else // Everything below will only be compiled if we are not on an Arduino
 
 #include <chrono>
@@ -12,6 +13,7 @@
 
 // Within here we must define the mock functions for the Arduino functions
 #include "serial_mock.h"
+#include "Adafruit_SPIFlash_mock.h"
 
 #include <thread>
 
@@ -20,22 +22,22 @@
 #define HIGH 1
 #define LOW 0
 
-void pinMode(int pin, int mode) {
+inline void pinMode(int pin, int mode) {
     // Do nothing
 }
 
-void digitalWrite(int pin, int value) {
+inline void digitalWrite(int pin, int value) {
     // Do nothing
 }
 
 
 // millis mock which still gives us the time since the program started in milliseconds
 static auto program_start = std::chrono::high_resolution_clock::now();
-unsigned long millis() {
+inline unsigned long millis() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - program_start).count();
 }
 
-void delay(unsigned long ms) {
+inline void delay(unsigned long ms) {
     // Wait using the real time clock
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
