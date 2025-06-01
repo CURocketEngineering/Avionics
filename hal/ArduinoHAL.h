f43cd8ca39c01deeadd2c369f262e529a9e1bfd2
Elan456 <telan4892@gmail.com>
2025-06-01 16:29:34 -0400

    chore: add no lint tag to mock

--- hal/ArduinoHAL.h
+++ hal/ArduinoHAL.h
@@ -47,7 +47,7 @@ inline unsigned long millis() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - program_start).count();
 }
 
-inline void delay(unsigned long ms) {
+inline void delay(unsigned long ms) { // NOLINT
     // Wait using the real time clock
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
