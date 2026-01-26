45f84d5ac1e201a8d0f0e6d58fbff79e7c4ff447
jstikel <jstikel@clemson.edu>
2026-01-26 17:41:56 +0000

    Deploying to gh-pages from @ CURocketEngineering/Avionics@33a848b708a5ed7cc51dd8aad7a456dd8ab78559 🚀

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['launchdeteched_0',['launchDeteched',['../classGroundLevelEstimator.html#acde5458a25b436c7ce3f8aadbeb88381',1,'GroundLevelEstimator']]],
-  ['launchdetected_1',['launchDetected',['../classIDataSaver.html#ab85dcc857f197475c14f9e50e9d34a97',1,'IDataSaver::launchDetected()'],['../classDataSaverSPI.html#aec6ba457bb1daac36ec1769001097631',1,'DataSaverSPI::launchDetected()']]],
-  ['launchdetector_2',['LaunchDetector',['../classLaunchDetector.html#a10e33e12876e820753cde55b85c6f6dc',1,'LaunchDetector']]]
+  ['launchdetected_0',['launchDetected',['../classIDataSaver.html#ab85dcc857f197475c14f9e50e9d34a97',1,'IDataSaver::launchDetected()'],['../classDataSaverSPI.html#aec6ba457bb1daac36ec1769001097631',1,'DataSaverSPI::launchDetected()'],['../classGroundLevelEstimator.html#a5993686a16bfa41f3e0fc793cf7b798c',1,'GroundLevelEstimator::launchDetected()']]],
+  ['launchdetector_1',['LaunchDetector',['../classLaunchDetector.html#a10e33e12876e820753cde55b85c6f6dc',1,'LaunchDetector']]]
 ];
