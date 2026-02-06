69106921121b177fef8276fccde8a1d9dd17920c
Jackson Shepherd <jshepherdpersonal@gmail.com>
2026-02-06 12:12:24 -0500

    fix: update comments in PowerManagement.h

--- include/PowerManagement.h
+++ include/PowerManagement.h
@@ -11,21 +11,25 @@
  */
 class BatteryVoltage {
     public:
+        /**
+         * @param adcPin The ADC pin connected to the battery voltage divider.
+         * @param factor The scaling factor to convert the pin voltage to battery voltage.
+         * @param numAdcBits The resolution of the ADC (e.g., 12 for 12-bit ADC).
+         */
         BatteryVoltage(uint8_t adcPin, float factor, int numAdcBits)
             : pin(adcPin), factor(factor), numAdcBits(numAdcBits) {analogReadResolution(numAdcBits);}
     
         /**
          * @brief Sample the ADC and convert to battery voltage.
          * @return Converted voltage reading.
-         * @note When to use: periodic health checks or brownout warnings.
          */
         float readVoltage() {
             // Set the pin for reading
             pinMode(pin, INPUT);
-            uint32_t rawValue = analogRead(pin);  // Should give a value between 0 and 4095
-            float vRef = 3.3f;
-            // Conversion notes ->  respect to Vref you would do Vref * analogRead()/2^12. 
-            // After that you can use the voltage divider equation to get the battery voltage level
+            uint32_t rawValue = analogRead(pin);  // Should give a value between 0 and 2^numAdcBits - 1
+            float vRef = 3.3f; // reference voltage for the ADC on MARTHA 1.4
+
+            // Convert raw ADC value to voltage at the pin, then apply the factor to get battery voltage
             float vPin = (static_cast<float>(rawValue) / (static_cast<float>(1 << numAdcBits) - 1)) * vRef;
             float vBat = vPin * factor;
 
