9b42842323c582aff9860d1476aa7103a1a6277c
Jackson Shepherd <jshepherdpersonal@gmail.com>
2026-02-05 19:36:56 -0500

    feat: update PowerManagement voltage calculation logic

--- include/PowerManagement.h
+++ include/PowerManagement.h
@@ -11,8 +11,8 @@
  */
 class BatteryVoltage {
     public:
-        BatteryVoltage(uint8_t adcPin, float conversionFactor)
-            : pin(adcPin), factor(conversionFactor) {}
+        BatteryVoltage(uint8_t adcPin, float factor, int numAdcBits)
+            : pin(adcPin), factor(factor), numAdcBits(numAdcBits) {analogReadResolution(numAdcBits);}
     
         /**
          * @brief Sample the ADC and convert to battery voltage.
@@ -22,14 +22,14 @@ class BatteryVoltage {
         float readVoltage() {
             // Set the pin for reading
             pinMode(pin, INPUT);
-            uint32_t rawValue = 0;
-            rawValue = analogRead(pin);  // Should give a value between 0 and 1023, not sure how to convert this to the true voltage. 
+            uint32_t rawValue = analogRead(pin);  // Should give a value between 0 and 4095
+            float vRef = 3.3f;
             // Conversion notes ->  respect to Vref you would do Vref * analogRead()/2^12. 
             // After that you can use the voltage divider equation to get the battery voltage level
-            // Vref is 134.33? 
-            // float voltage = pow((rawValue/2),12) * factor; 
-            Serial.println(rawValue);
-            return rawValue;
+            float vPin = (static_cast<float>(rawValue) / (static_cast<float>(1 << numAdcBits) - 1)) * vRef;
+            float vBat = vPin * factor;
+
+            return vBat;
         }
       
         /**
@@ -45,6 +45,7 @@ class BatteryVoltage {
     private:
         uint8_t pin;
         float factor;
+        int numAdcBits;
     };
     
     #endif // PowerManagement_H
