943a75d24866ebe66d4fa0620bf10e7393d99c69
Jshepherd06 <jshepherdpersonal@gmail.com>
2026-02-07 19:32:54 +0000

    Deploying to gh-pages from @ CURocketEngineering/Avionics@12e9db62eee930f7b24fa500b191358a3ac07d8a 🚀

--- classBatteryVoltage.js
+++ classBatteryVoltage.js
@@ -1,6 +1,6 @@
 var classBatteryVoltage =
 [
-    [ "BatteryVoltage", "classBatteryVoltage.html#a6b8b9c7eb7fcf9c99d995ffd43b7caed", null ],
-    [ "isAlive", "classBatteryVoltage.html#aab9f49c1731aaccd4072b742c8cbea6f", null ],
+    [ "BatteryVoltage", "classBatteryVoltage.html#ab55a05a6c577d769bcd76b782b6b7424", null ],
+    [ "isLow", "classBatteryVoltage.html#a92adbc9d223f27da1d3ec769ce07a5b9", null ],
     [ "readVoltage", "classBatteryVoltage.html#a0b863d8ac1103926797c004ac152a475", null ]
 ];
