0af85e16ff9979fd07c534ac0ac2dc2d4c3a873c
Elan456 <telan4892@gmail.com>
2026-03-12 16:04:02 -0400

    ref: redistrbute constant config defs amongst sim sensors

--- include/simulation/Serial_Sim_LIS2MDL.h
+++ include/simulation/Serial_Sim_LIS2MDL.h
@@ -3,6 +3,10 @@
 
 #include "Serial_Sim.h"
 
+#ifndef LIS2MDL_RATE_100_HZ
+#define LIS2MDL_RATE_100_HZ 0x06
+#endif
+
 class Adafruit_LIS2MDL {
 public:
     Adafruit_LIS2MDL (){}
@@ -10,15 +14,18 @@ public:
     bool begin_SPI(int cs) { return true; } // Mock successful initialization SPI
     bool begin(int addr) { return true; } // Mock successful initialization I2C
 
-    void setDataRate(int rate) {}
+    void setDataRate(int rate) { dataRate = rate; }
     void enableInterrupts(bool a) {}
 
-    int getDataRate() { return 100; } // Mock as 155 Hz
+    int getDataRate() { return dataRate; }
 
     void getEvent(sensors_event_t *mag) {
         SerialSim::getInstance().updateMag(mag);
 
     }
+
+private:
+    int dataRate = LIS2MDL_RATE_100_HZ;
 
 };
 
