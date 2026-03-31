7745011641175f7924f4c4a1e5d6a850d4a8db4e
Elan456 <telan4892@gmail.com>
2026-03-31 12:57:52 -0400

    docs: fix spelling mistake for necessarily

--- include/state_estimation/States.h
+++ include/state_estimation/States.h
@@ -15,7 +15,7 @@
 // apogee is an event that causes a transition from STATE_ASCENT to STATE_DESCENT, but it is not a state itself.
 // The state machines are in charge of detecting these transitions and updating the states. 
 enum FlightState : uint8_t {
-    STATE_UNARMED = 0,      // 0 This may not neccessarily always be the 0th state, or the first state. A state could be added before it.
+    STATE_UNARMED = 0,      // 0 This may not necessarily always be the 0th state, or the first state. A state could be added before it.
     STATE_ARMED,            // 1
     STATE_SOFT_ASCENT,      // 2
     STATE_ASCENT,           // 3 Don't use the ascent state if you are already using powered ascent and coast ascent
